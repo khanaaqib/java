08c6bb396ae386d1e30568750e90489ae051fbf7
khanaaqib <35225284+khanaaqib@users.noreply.github.com>
2020-03-05 02:56:40 +0530

    CMT:removing the other content and using src

---
+++ src/test/java/mavenfirst/HelloMaven/AppTest.java
@@ -0,0 +1,20 @@
+package mavenfirst.HelloMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
